a5c13af6f6c6923c8df4350784ebcfee1c3a4e9f
chen <jc.ustb@163.com>
2019-12-03 21:07:28 +0800

    phase01

---
+++ src/xyz/chen/BaseRequest.java
@@ -0,0 +1,18 @@
+package xyz.chen;
+
+/**
+ * Description:
+ * Author: chen
+ * 2019/12/3
+ */
+public class BaseRequest {
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
